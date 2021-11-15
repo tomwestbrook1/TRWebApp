14186f86f29f954d1345deb8af37976aa9f1f251
TRW <tom.westbrook@live.com>
2021-11-15 23:33:57 +0000

    adding python cgi and html form

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 # imports ....
+import cgi
+import cgitb
+cgitb.enable(display=0, logdir='./Files/log') # unsure on args at this point
 
 # vars ....
+form = cgi.FieldStorage()
+searchItem = form.getvalue('txtinput')
 
+open_file = open('./Files/outputs', 'a')
+
+open_file.write("Hello World")
+open_file.write(searchItem)
+open_file.close()
+
+
+"""
 def main():
     print("Hello World")
 
@@ -9,3 +22,5 @@ def main():
 
 if __name__ == "__main__": 
     main()
+
+"""
