3d17f85f4314b2b64d1d43a1f21a817177c5d63b
TRW <tom.westbrook@live.com>
2021-11-16 22:24:49 +0000

    page navigation

--- README.md
+++ README.md
@@ -9,4 +9,13 @@ Tasks so far:
     - Install Flask???
 - Create webpage with some form of state (when you add items to the page they remain after closing the page)
     - This bit won't be that hard, just need to be able to trigger the script from the html page
-- 
+-
+
+Notes:
+- When running Flask on my machine it is in debug mode (export FLASK_ENV=development)
+    - The benefit of doing this is that when you get an error it will show you the stack trace on the webpage
+- Using the action="" will send you to the page with that path
+    - this means that you
+- Now have working page navigation using Flask template between pages
+
+
--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,50 @@
 # imports ....
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import webbrowser
+from markupsafe import escape
 
-app = Flask(__name__)
+# variables....
+app = Flask(__name__, static_url_path='')
+
+# functions...
+
+
 @app.route("/")
+def main_page():
+    return render_template("ContentHome.html")
+
+
+@app.route("/home")
 def home():
     return render_template("ContentHome.html")
 
-@app.route('/', methods=['POST'])
-def handle_form():
-    print("here")
-    return (request.form['txtinput'])
+
+@app.route("/about")
+def about():
+    return render_template("About.html")
+
+
+@app.route("/references")
+def references():
+    return render_template("References.html")
+
+
+@app.route("/<name>")
+def slash_name(name):
+    return f"hello, {escape(name)}!"
+
+
+@app.route('/action_form', methods=['POST'])
+def action_form():
+    # -> prints the output to the terminal along with the flask data
+    print(request.form['txtinput'] + " attachment")
+    response = request.form['txtinput']
+
+    # -> sends the content back to the webpage
+    return f"hello, {escape(response)}!"
 
 # functions
 
-if __name__ == "__main__": 
-    app.run()
 
+if __name__ == "__main__":
+    app.run()
